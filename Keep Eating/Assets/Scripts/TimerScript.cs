e43de6bd0a33c14c5984a25b8eca6d5ad2bbf09c
tuh04152 <tuh04152@temple.edu>
2021-10-10 20:19:38 -0400

    Updated Timer Script to update a GameObject

--- Keep Eating/Assets/Scripts/TimerScript.cs
+++ Keep Eating/Assets/Scripts/TimerScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
     private float timer;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,7 @@ public class TimerScript : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
+        text.text = timer.ToString();
 
         if (timer <= 0)
         {
