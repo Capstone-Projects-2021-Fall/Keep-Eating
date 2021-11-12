c74f0c0309937436782cb4dcbf19ff58f6e608a8
ShigureChen <tuk93581@temple.edu>
2021-11-11 20:54:13 -0500

    update while player change

--- Keep Eating/Assets/Scripts/StatusUpdate.cs
+++ Keep Eating/Assets/Scripts/StatusUpdate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,31 +8,45 @@ using Photon.Pun;
 public class StatusUpdate : MonoBehaviour
 {
     public Text PlayerCountNumber;
+    private int pre1 = 0;
+    private int temp1 = 0;
     public Text RoomCountNumber;
+    private int pre2 = 0;
+    private int temp2 = 0;
     public Text PlayerInRoomNumber;
+    private int pre3 = 0;
+    private int temp3 = 0;
     public Text PlayerInMasterNumber;
-    private int counter = 0;
-    private int playerCount;
-    private int playerMasterCount;
+    private int pre4 = 0;
+    private int temp4 = 0;
 
 
     // Update is called once per frame
     private void Update()
     {
-        playerCount = PhotonNetwork.CountOfPlayers - 1;
-        playerMasterCount = PhotonNetwork.CountOfPlayersOnMaster - 1;
-        PlayerCountNumber.text = playerCount.ToString();
-        RoomCountNumber.text = PhotonNetwork.CountOfRooms.ToString();
-        PlayerInRoomNumber.text = PhotonNetwork.CountOfPlayersInRooms.ToString();
-        PlayerInMasterNumber.text = playerMasterCount.ToString();
-        
-        counter++;
-        if(counter == 300)
+        pre1 = PhotonNetwork.CountOfPlayers - 1;
+        pre2 = PhotonNetwork.CountOfRooms;
+        pre3 = PhotonNetwork.CountOfPlayersInRooms;
+        pre4 = PhotonNetwork.CountOfPlayersOnMaster - 1;
+
+        if(pre1 != temp1 || pre2 != temp2 || pre3 != temp3 || pre4 != temp4)
         {
-            Debug.Log(PhotonNetwork.CountOfPlayers.ToString() + PhotonNetwork.CountOfRooms.ToString() + 
-                        PhotonNetwork.CountOfPlayersInRooms.ToString() + PhotonNetwork.CountOfPlayersOnMaster.ToString());
-            counter = 0;
+            temp1 = pre1;
+            temp2 = pre2;
+            temp3 = pre3;
+            temp4 = pre4;
+            refresh();
         }
-        
+    }
+
+    private void refresh()
+    {
+        PlayerCountNumber.text = temp1.ToString();
+        RoomCountNumber.text = temp2.ToString();
+        PlayerInRoomNumber.text = temp3.ToString();
+        PlayerInMasterNumber.text = temp4.ToString();
+
+        Debug.Log("On time:" + DateTime.Now.ToString("HH:mm:ss tt") + " Player or room number changed!");
+        Debug.Log("players: " + temp1 + " rooms: " + temp2 + " player in room: "+ temp3 + " player in main: "+ temp4);
     }
 }
